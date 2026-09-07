a7b40ecb06c557b347da85714c3a967110e65f47
Alfonso Calvente <alfonsoclpez@gmail.com>
2026-09-07 20:50:43 +0200

    Apertura de proyecto: To-Do-List

---
+++ src/main/java/com/menhloh/quehacer/QuehacerApplication.java
@@ -0,0 +1,13 @@
+package com.menhloh.quehacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuehacerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuehacerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/menhloh/quehacer/QuehacerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menhloh.quehacer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuehacerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
